bb1405cb8aca709eac50b6c72a438fd1b37df807
Pepe Valdivia <jvaldivia@softweb.pe>
2017-09-05 17:10:49 -0500

    Commit inicial

---
+++ app/src/main/java/pe/edu/ulima/idic/quinua/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package pe.edu.ulima.idic.quinua.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pe.edu.ulima.idic.quinua.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
